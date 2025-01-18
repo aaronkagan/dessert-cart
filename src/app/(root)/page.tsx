b1d108bdfafde7d18202a3b4148b7f06f1520e51
Aaron Kagan <aaronkagan@gmail.com>
2025-01-18 18:22:08 -0500

    changing layout

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,6 +1,7 @@
 import DesertCard from '@/components/desert-card';
 import { Metadata } from 'next';
 import { data as products } from '../../../public/data';
+import Cart from '@/components/cart/cart';
 
 export const metadata: Metadata = {
   title: 'Home',
@@ -10,12 +11,14 @@ export default function Home() {
   return (
     <div className="max-w-[90%] mx-auto ">
       <h1 className="font-bold text-2xl">Desserts</h1>
-      <section className="grid md:grid-cols-4 gap-5 mt-5 max-w-[60%]">
-        {products.map((product) => (
-          <DesertCard key={product.name} product={product}></DesertCard>
-        ))}
-      </section>
-      <aside></aside>
+      <div className="grid grid-cols-3 gap-10 mt-5">
+        <section className="grid md:grid-cols-4 gap-5 col-span-2">
+          {products.map((product) => (
+            <DesertCard key={product.name} product={product}></DesertCard>
+          ))}
+        </section>
+        <Cart />
+      </div>
     </div>
   );
 }

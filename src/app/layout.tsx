1c8bb3d040fd90b3701d96db2fc4433467cba667
Aaron Kagan <aaronkagan@gmail.com>
2025-01-17 21:55:49 -0500

    fixing layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <div className="w-[80%] mx-auto">{children}</div>
       </body>
     </html>
   );

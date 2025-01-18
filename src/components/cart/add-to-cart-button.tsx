bb9c550ce4b0aecc35b1e3ff12f45ba7b78c5dc2
Aaron Kagan <aaronkagan@gmail.com>
2025-01-18 16:36:55 -0500

    getting quantity from cart

--- src/components/cart/add-to-cart-button.tsx
+++ src/components/cart/add-to-cart-button.tsx
@@ -1,10 +1,23 @@
-export default function AddToCartButton({ qty }: { qty: number }) {
+'use client';
+
+import { useAppSelector } from '@/lib/hooks';
+import { selectCart } from '@/lib/features/cart/cart-slice';
+import { Product } from '@/types';
+
+export default function AddToCartButton({ product }: { product: Product }) {
+  const cart = useAppSelector(selectCart);
+
+  const cartQty = cart.reduce(
+    (_, currVal) => (currVal.name === product.name ? currVal.qty : 0),
+    0
+  );
+
   return (
     <div className="border w-[10rem] rounded-full px-4 flex justify-center">
-      {qty > 0 ? (
+      {cartQty > 0 ? (
         <button className="flex gap-5 items-center">
           <span>-</span>
-          {qty} <span>+</span>
+          {cartQty} <span>+</span>
         </button>
       ) : (
         <button>+ Add To Cart</button>

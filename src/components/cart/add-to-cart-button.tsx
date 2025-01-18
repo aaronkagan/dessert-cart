04e247412b21d0e5df79dbf0ffaafde58eb73b17
Aaron Kagan <aaronkagan@gmail.com>
2025-01-18 18:22:25 -0500

    dispatching actions

--- src/components/cart/add-to-cart-button.tsx
+++ src/components/cart/add-to-cart-button.tsx
@@ -1,26 +1,55 @@
 'use client';
 
-import { useAppSelector } from '@/lib/hooks';
-import { selectCart } from '@/lib/features/cart/cart-slice';
+import { useAppDispatch, useAppSelector } from '@/lib/hooks';
+import {
+  addToCart,
+  decreaseQty,
+  increaseQty,
+  removeFromCart,
+  selectCart,
+} from '@/lib/features/cart/cart-slice';
 import { Product } from '@/types';
 
 export default function AddToCartButton({ product }: { product: Product }) {
   const cart = useAppSelector(selectCart);
+  const dispatch = useAppDispatch();
 
-  const cartQty = cart.reduce(
-    (_, currVal) => (currVal.name === product.name ? currVal.qty : 0),
-    0
+  let cartQty = 0;
+
+  cart.forEach((elem) =>
+    elem.name === product.name ? (cartQty = elem.qty) : null
   );
 
   return (
     <div className="border w-[10rem] rounded-full px-4 flex justify-center">
       {cartQty > 0 ? (
         <button className="flex gap-5 items-center">
-          <span>-</span>
-          {cartQty} <span>+</span>
+          <span
+            onClick={() => {
+              cartQty > 1
+                ? dispatch(decreaseQty(product.name))
+                : dispatch(removeFromCart(product.name));
+            }}
+          >
+            -
+          </span>
+          {cartQty}
+          <span
+            onClick={() => {
+              dispatch(increaseQty(product.name));
+            }}
+          >
+            +
+          </span>
         </button>
       ) : (
-        <button>+ Add To Cart</button>
+        <button
+          onClick={() => {
+            dispatch(addToCart(product));
+          }}
+        >
+          + Add To Cart
+        </button>
       )}
     </div>
   );

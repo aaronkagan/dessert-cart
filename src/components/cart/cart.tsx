a32ba24433665902c4e0a8658560cbc6c0177ebe
Aaron Kagan <aaronkagan@gmail.com>
2025-01-23 18:18:52 -0500

    implementing redux toolkit

--- src/components/cart/cart.tsx
+++ src/components/cart/cart.tsx
@@ -1,17 +1,40 @@
 'use client';
 
-import { selectCart } from '@/lib/features/cart/cart-slice';
-import { useAppSelector } from '@/lib/hooks';
+import { removeFromCart, selectCart } from '@/lib/features/cart/cart-slice';
+import { useAppDispatch, useAppSelector } from '@/lib/hooks';
+import Image from 'next/image';
 
 export default function Cart() {
   const cart = useAppSelector(selectCart);
+  const dispatch = useAppDispatch();
 
   return (
     <aside className="w-full">
       <h2>Your Cart ({cart.length})</h2>
       <section>
         {cart.length > 0 ? (
-          cart.map((item) => <div key={item.name}>{JSON.stringify(item)}</div>)
+          cart.map((item) => (
+            <div key={item.name} className="flex gap-4">
+              <div className="flex justify-between w-full">
+                <table className="w-full">
+                  <tr className="flex justify-between">
+                    <td>{item.name}</td>
+                    <td className="bg-red-50">${item.price.toFixed(2)}</td>
+                    <td>{item.qty}</td>
+                  </tr>
+                </table>
+              </div>
+              <button onClick={() => dispatch(removeFromCart(item.name))}>
+                <Image
+                  src="/images/icon-remove-item.svg"
+                  className="w-4 h-4"
+                  width={50}
+                  height={50}
+                  alt="remove item"
+                />
+              </button>
+            </div>
+          ))
         ) : (
           <p>Your cart is empty</p>
         )}
---
+++ src/lib/features/cart/cart-slice.ts
@@ -0,0 +1,41 @@
+import { RootState } from '@/lib/store';
+import { CartItem, Product } from '@/types';
+import { createSlice } from '@reduxjs/toolkit';
+import type { PayloadAction } from '@reduxjs/toolkit';
+
+const initialState: CartItem[] = [];
+
+export const cartSlice = createSlice({
+  name: 'cart',
+  initialState,
+  reducers: {
+    addToCart: (state, action: PayloadAction<Product>) => {
+      state.push({
+        name: action.payload.name,
+        price: action.payload.price,
+        qty: 1,
+      });
+    },
+    // Using the item name as the payload
+    removeFromCart: (state, action: PayloadAction<string>) => {
+      return state.filter((item) => item.name !== action.payload);
+    },
+    // Using the item name as the payload
+    increaseQty: (state, action: PayloadAction<string>) => {
+      return state.map((item) =>
+        item.name === action.payload ? { ...item, qty: item.qty + 1 } : item
+      );
+    },
+    // Using the item name as the payload
+    decreaseQty: (state, action: PayloadAction<string>) => {
+      return state.map((item) =>
+        item.name === action.payload ? { ...item, qty: item.qty - 1 } : item
+      );
+    },
+  },
+});
+
+export const { addToCart, removeFromCart, increaseQty, decreaseQty } =
+  cartSlice.actions;
+export const selectCart = (state: RootState) => state.cart;
+export default cartSlice.reducer;

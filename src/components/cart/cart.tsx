0a22b814bd643b0d1ad10f9500ed57c28c4e5b1c
Aaron Kagan <aaronkagan@gmail.com>
2025-01-24 11:28:22 -0500

    adding cart elements

--- src/components/cart/cart.tsx
+++ src/components/cart/cart.tsx
@@ -3,6 +3,7 @@
 import { removeFromCart, selectCart } from '@/lib/features/cart-slice';
 import { resetStock } from '@/lib/features/products-slice';
 import { useAppDispatch, useAppSelector } from '@/lib/hooks';
+import { getCartTotal } from '@/utils';
 import Image from 'next/image';
 
 export default function Cart() {
@@ -16,46 +17,64 @@ export default function Cart() {
       </h2>
       <section>
         {cart.length > 0 ? (
-          cart.map((item) => (
-            <div
-              key={item.name}
-              className="flex gap-4 border-b-[1px] py-1 mt-2"
-            >
-              <div className="flex justify-between w-full">
-                <div className="w-full">
-                  <div className="flex flex-col gap-1">
-                    <p className="font-bold text-sm">{item.name}</p>
-                    <div className="flex items-center">
-                      <p className="mr-5 text-[#b93c11] font-semibold">
-                        {item.qty}x
-                      </p>
-                      <p className="mr-2 opacity-50">
-                        <span className="text-xs">@</span>$
-                        {item.price.toFixed(2)}
-                      </p>
-                      <p className="font-semibold">
-                        ${(item.price * item.qty).toFixed(2)}
-                      </p>
+          <div>
+            {cart.map((item) => (
+              <div
+                key={item.name}
+                className="flex gap-4 border-b-[1px] py-1 mt-2"
+              >
+                <div className="flex justify-between w-full">
+                  <div className="w-full">
+                    <div className="flex flex-col gap-1">
+                      <p className="font-bold text-sm">{item.name}</p>
+                      <div className="flex items-center">
+                        <p className="mr-5 text-[#b93c11] font-semibold">
+                          {item.qty}x
+                        </p>
+                        <p className="mr-2 opacity-50">
+                          <span className="text-xs">@</span>$
+                          {item.price.toFixed(2)}
+                        </p>
+                        <p className="font-semibold">
+                          ${(item.price * item.qty).toFixed(2)}
+                        </p>
+                      </div>
                     </div>
                   </div>
                 </div>
+                <button
+                  onClick={() => {
+                    dispatch(removeFromCart(item.name));
+                    dispatch(resetStock(item.name));
+                  }}
+                >
+                  <Image
+                    src="/images/icon-remove-item.svg"
+                    className="w-3 h-3"
+                    width={50}
+                    height={50}
+                    alt="remove item"
+                  />
+                </button>
               </div>
-              <button
-                onClick={() => {
-                  dispatch(removeFromCart(item.name));
-                  dispatch(resetStock(item.name));
-                }}
-              >
-                <Image
-                  src="/images/icon-remove-item.svg"
-                  className="w-3 h-3"
-                  width={50}
-                  height={50}
-                  alt="remove item"
-                />
-              </button>
+            ))}
+            <div className="flex justify-between pt-4">
+              <p>Order Total</p>
+              <p className="font-extrabold text-xl">${getCartTotal(cart)}</p>
             </div>
-          ))
+            <div className="flex items-center justify-center gap-1 bg-[#fcf8f6] rounded-lg py-2 my-2">
+              <Image
+                src="/images/icon-carbon-neutral.svg"
+                alt=""
+                width={15}
+                height={15}
+              />
+              <p className="text-xs">This is a carbon neutral order</p>
+            </div>
+            <button className="text-white text-sm px-4 py-2 border rounded-full w-full justify-center bg-[#c43d10] mt-2">
+              Confirm Order
+            </button>
+          </div>
         ) : (
           <div className="flex flex-col items-center mt-5">
             <Image

8e3dc72411807ce9b07e8ae9cf44227fc5f3aa9e
Aaron Kagan <aaronkagan@gmail.com>
2025-01-24 11:04:45 -0500

    styling

--- src/components/cart/add-to-cart-button.tsx
+++ src/components/cart/add-to-cart-button.tsx
@@ -22,9 +22,9 @@ export default function AddToCartButton({ product }: { product: Product }) {
   const cartQty = getCartQty(product.name);
 
   return (
-    <div className="border border-[#231818] w-[10rem] rounded-full px-4 py-2 flex justify-center">
+    <div className="w-[10rem] rounded-full flex justify-center">
       {cartQty > 0 ? (
-        <button className="flex gap-5 items-center">
+        <button className="flex w-full justify-between items-center bg-[#c43d10] text-white rounded-full px-4 py-2">
           <span
             onClick={() => {
               if (cartQty > 1) {
@@ -35,8 +35,16 @@ export default function AddToCartButton({ product }: { product: Product }) {
                 dispatch(resetStock(product.name));
               }
             }}
+            className="hover:bg-white rounded-full w-4 h-4 flex items-center justify-center border"
           >
-            -
+            {' '}
+            <Image
+              width={8}
+              height={8}
+              src="/images/icon-decrement-quantity.svg"
+              alt="increment quantity"
+              className="hover:text-black"
+            />
           </span>
           {cartQty}
           <span
@@ -46,8 +54,14 @@ export default function AddToCartButton({ product }: { product: Product }) {
                 dispatch(decreaseStock(product.name));
               }
             }}
+            className="hover:bg-white rounded-full w-4 h-4 flex items-center justify-center border"
           >
-            +
+            <Image
+              width={8}
+              height={8}
+              src="/images/icon-increment-quantity.svg"
+              alt="increment quantity"
+            />
           </span>
         </button>
       ) : (
@@ -58,7 +72,7 @@ export default function AddToCartButton({ product }: { product: Product }) {
               dispatch(decreaseStock(product.name));
             }
           }}
-          className="text-[#231818] font-bold flex items-center gap-2 text-sm"
+          className="text-[#231818] font-bold flex items-center gap-2 text-sm px-4 py-2 border rounded-full w-full justify-center border-[#c43d10]"
         >
           <Image
             src="/images/icon-add-to-cart.svg"

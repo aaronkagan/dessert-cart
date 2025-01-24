28840468f7409be07c1c4a04617c3ddc757e5521
Aaron Kagan <aaronkagan@gmail.com>
2025-01-24 11:31:33 -0500

    styling

--- src/components/desert-card.tsx
+++ src/components/desert-card.tsx
@@ -13,18 +13,20 @@ export default function DesertCard({ product }: { product: Product }) {
         priority={true}
         className="w-full object-cover rounded-lg"
       />
-      <div className="mx-auto flex justify-center my-4 -translate-y-9 bg-white w-fit rounded-full">
-        <AddToCartButton product={product} />
-      </div>
-      <div>
-        <h3 className="text-[#978a86]">{product.category}</h3>
-        <h2 className="text-[#231818] font-bold">{product.name}</h2>
-        <p className="text-[#ad6b55] font-semibold">
-          ${product.price.toFixed(2)}
-        </p>
-        <p className={product.stock === 0 ? 'text-red-500' : ''}>
-          Stock: {product.stock}
-        </p>
+      <div className="-translate-y-5">
+        <div className="mx-auto flex justify-center mb-4  bg-white w-fit rounded-full">
+          <AddToCartButton product={product} />
+        </div>
+        <div>
+          <h3 className="text-[#978a86]">{product.category}</h3>
+          <h2 className="text-[#231818] font-bold">{product.name}</h2>
+          <p className="text-[#ad6b55] font-semibold">
+            ${product.price.toFixed(2)}
+          </p>
+          <p className={product.stock === 0 ? 'text-red-500' : ''}>
+            Stock: {product.stock}
+          </p>
+        </div>
       </div>
     </article>
   );
